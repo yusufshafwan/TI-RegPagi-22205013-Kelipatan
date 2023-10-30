669ba33cd31cbeb2f267993080c0416b442bfc5f
yusufshafwan <149211741+yusufshafwan@users.noreply.github.com>
2023-10-30 11:36:58 +0700

    Commit

---
+++ latihan18.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class latihan18 {
+ public static void main(String[] args){
+    System.out.println("Bilangan kelipatan 3 dan 5 dari 3 sampai 35:");
+        for (double i = 3.5; i <= 35; i+=3.5){
+            System.out.println(i);
+            
+        }
+    }
+}
